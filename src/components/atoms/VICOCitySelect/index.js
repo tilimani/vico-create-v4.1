58d960129dd1fbabc20c8be0da9f4135b0bb41ac
tilimani <33944497+tilimani@users.noreply.github.com>
2020-08-25 09:32:25 +0200

    initial commit

--- src/components/atoms/VICOCitySelect/index.js
+++ src/components/atoms/VICOCitySelect/index.js
@@ -1,109 +1,25 @@
 import React, { useState } from "react";
-import {
-  InputLabel,
-  FormControl,
-  NativeSelect,
-  makeStyles,
-  fade
-} from "@material-ui/core";
-import clsx from "clsx";
-import { MuiThemeProvider } from "@material-ui/core/styles";
-import theme from "../../../common/theme";
+import VICOSelect from "../VICOSelect";
 
-const useStylesVICO = makeStyles((theme) => ({
-  padding: {
-    paddingLeft: theme.spacing(1.5)
-  },
-  inputLabel: {
-    "&[data-shrink^='true']": {
-      transform: "translate(12px, 6px) scale(0.75)"
-    }
-  },
-  root: {
-    border: "1px solid #e2e2e1",
-    marginBottom: "1rem",
-    overflow: "hidden",
-    height: "54px",
-    borderRadius: 4,
-    backgroundColor: "#fcfcfb",
-    transition: theme.transitions.create(["border-color", "box-shadow"]),
-    "&:hover": {
-      backgroundColor: "#fff"
-    },
-    "&$focused": {
-      backgroundColor: "#fff",
-      boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
-      borderColor: theme.palette.primary.main
-    },
-    "&.MuiNativeSelect-filled": {
-      padding: "27px 12px 10px"
-    }
-  },
-  focused: {},
-  fullWidth: {
-    width: "100%"
-  },
-  underlined: {
-    border: "0px solid",
-    color: "#ef8e01",
-    borderRadius: "0px",
-    backgroundColor: "white",
-    borderBottom: "1px solid " + theme.palette.primary.main,
-    height: "24px",
-    "& $inputLabel": {
-      display: "none"
-    },
-    "& $padding": {
-      paddingLeft: "8px",
-      marginTop: "-4px",
-      color: "#ef8e01"
-    },
-    "& svg": {
-      color: theme.palette.primary.main
-    }
-  }
-}));
 
-function VICOSelect(props) {
+function VICOCitySelect(props) {
   const [city, setCity] = useState("");
-  const classes = useStylesVICO();
   const cityOptions = [
     { value: "medellin", label: "Medellín" },
     { value: "bogota", label: "Bogotá" }
   ];
-
   const handleCityChange = (e) => {
     setCity(e.target.value);
   };
-  console.log(props.fullWidth);
   return (
-    <MuiThemeProvider theme={theme}>
-      <FormControl
-        variant="outlined"
-        className={clsx([classes.root], {
-          [classes.fullWidth]: props.fullWidth,
-          [classes.underlined]: props.underlined
-        })}
-      >
-        <InputLabel className={classes.inputLabel}>{props.label}</InputLabel>
-        <NativeSelect
-          className={classes.padding}
+        <VICOSelect
           {...props}
           disableUnderline={true}
           options={cityOptions}
           value={city}
           onChange={handleCityChange}
-        >
-          {/*<option aria-label="None" value="" />*/}
-          {cityOptions.map((item, i) => (
-            <>
-              <option value={item.value}>{item.label}</option>
-            </>
-          ))}
-        </NativeSelect>
-      </FormControl>
-    </MuiThemeProvider>
+        />
   );
 }
 
-export default VICOSelect;
+export default VICOCitySelect;
--- src/components/pages/Assessment/Assessment.js
+++ src/components/pages/Assessment/Assessment.js
@@ -4,22 +4,87 @@ import Budget from "./Budget";
 import SplashScreen from "./SplashScreen";
 import Preferences from "./Preferences";
 import Recommendations from "./Recommendations";
-import Grid from "@material-ui/core";
 import { Route, Switch } from "react-router-dom";
+
+import { useHistory } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import CloseIcon from "@material-ui/icons/Close";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    [theme.breakpoints.up("sm")]: {
+      padding: "2rem",
+      background: "#212529",
+      position: "fixed",
+      width: "100%",
+      height: "100%",
+      left: "0",
+      top: "0"
+    }
+  },
+  container__inside: {
+    background: "white",
+    [theme.breakpoints.up("sm")]: {
+      width: "60%",
+      height: "80%"
+    }
+  },
+  container__header: {
+    padding: "1rem",
+    color: "white",
+    //background: theme.palette.primary.main,
+    background: "#ef8e01",
+    textAlign: "center",
+    [theme.breakpoints.up("sm")]: {},
+    "& img": {
+      maxHeight: "32px"
+    }
+  }
+}));
+
 const Assessment = () => {
+  let history = useHistory();
+  const classes = useStyles();
   return (
     <>
-      <Switch>
-        <Route exact path="/assessment/" component={Areas} />
-        <Route path="/assessment/budget" component={Budget} />
-        <Route path="/assessment/preferences" component={Preferences} />
-        <Route exact path="/assessment/loading" component={SplashScreen} />
-        <Route
-          exact
-          path="/assessment/recommendations"
-          component={Recommendations}
-        />
-      </Switch>
+      <Grid container className={classes.container}>
+        <Grid container className={classes.container__inside}>
+          <Grid
+            container
+            className={classes.container__header}
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Grid item xs={1}>
+              <ArrowBackIosIcon onClick={() => history.goBack()} />
+            </Grid>
+            <Grid item xs={10}>
+              <img
+                src="https://uploads.codesandbox.io/uploads/user/129a52fa-24c5-45b6-8b1e-048cf0197deb/X7w8-logo.png"
+                alt="logo"
+                id="vico-logo"
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CloseIcon onClick={() => history.goBack()} />
+            </Grid>
+          </Grid>
+          <Switch>
+            <Route exact path="/assessment/" component={Areas} />
+            <Route path="/assessment/budget" component={Budget} />
+            <Route path="/assessment/preferences" component={Preferences} />
+            <Route exact path="/assessment/loading" component={SplashScreen} />
+            <Route
+              exact
+              path="/assessment/recommendations"
+              component={Recommendations}
+            />
+          </Switch>
+        </Grid>
+      </Grid>
     </>
   );
 };
