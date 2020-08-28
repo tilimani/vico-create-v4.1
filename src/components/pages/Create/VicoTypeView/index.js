747282e828209b438fca3addb917cc98c64fb99e
oumaymaazouz <58980203+oumaymaazouz@users.noreply.github.com>
2020-08-28 11:30:00 +0300

    wip1

--- src/components/pages/Create/VicoTypeView/index.js
+++ src/components/pages/Create/VicoTypeView/index.js
@@ -1,20 +1,184 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
-import VICOButton from "../../../atoms/VICOButton";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { makeStyles } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 
-const VicoTypeView = () => {
+import VICOHouseTypeItem from "./VICOHouseTypeItem";
+import VICOReturnButton from "../../../atoms/VICOReturnButton";
+import { CreateConsumer } from "../../../../common/context";
+import VICOMobileLinearProgress from "../../../atoms/VICOMobileLinearProgress";
+
+const useStyles = makeStyles((theme) => ({
+  vicoTypeViewWrapper: {
+    [theme.breakpoints.down("md")]: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "98vh",
+      padding: "20px 30px 0px"
+    },
+    [theme.breakpoints.down("xs")]: {
+      display: "block",
+      padding: 0
+    }
+  },
+  vicoTypeViewContent: {
+    boxShadow: "0px 5px 4px 0px #AEA7A1",
+    borderRadius: 8,
+    padding: "40px 50px 50px",
+    margin: 140,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      boxShadow: "none",
+      margin: "auto",
+      display: "block",
+      padding: 0
+    }
+  },
+  vicoTypeViewTitle: {
+    fontSize: 36,
+    color: theme.palette.secondary.main,
+    fontWeight: "bold"
+  },
+  vicoTypeViewSubtitle: {
+    color: theme.palette.secondary.main,
+    fontSize: 16,
+    width: 480,
+    marginTop: 20,
+    [theme.breakpoints.down("sm")]: {
+      width: "auto"
+    }
+  },
+  gridContainer: {
+    marginTop: 40,
+    [theme.breakpoints.down("sm")]: {
+      width: "auto",
+      margin: "auto"
+    }
+  },
+  backText: {
+    fontSize: 18,
+    color: theme.palette.secondary.main,
+    marginLeft: 10
+  },
+  innerContent: {
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 20,
+      padding: "0px 30px"
+    }
+  },
+  noteWrapper: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginTop: 20,
+    cursor: "pointer",
+    position: "relative",
+    marginBottom: 20
+  },
+  note: {
+    color: theme.palette.secondary.main,
+    fontSize: 20,
+    fontWeight: "bold",
+    float: "right",
+    width: 300,
+    textAlign: "right",
+    marginRight: 20,
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+      textAlign: "left"
+    }
+  },
+  mobileRightArrow: {
+    marginLeft: 10,
+    position: "absolute",
+    top: 30
+  }
+}));
+const vicoTypeView = (props) => {
+  const classes = useStyles();
+  const isMediumScreen = useMediaQuery("(max-width:960px)");
+  const isMobileScreen = useMediaQuery("(max-width:600px)");
+  const buttonWidth = isMediumScreen ? "100%" : 267;
+
   return (
-    <>
-      Vico Type 1
-      <VICOButton
-        component={RouterLink}
-        to="/create/name"
-        variant="contained"
-        color="primary"
-        text="Continue"
-      />
-    </>
+    <div className={classes.vicoTypeViewWrapper}>
+      <div className={classes.vicoTypeViewContent}>
+        {isMediumScreen && (
+          <CreateConsumer>
+            {(state) => {
+              return (
+                <div>
+                  <VICOMobileLinearProgress step={state.createStep} />
+                  <VICOReturnButton
+                    action={() => {
+                      props.history.push("/create/howto");
+                      state.changeState("createStep", 0);
+                    }}
+                  />
+                </div>
+              );
+            }}
+          </CreateConsumer>
+        )}
+        <div className={classes.innerContent}>
+          <span className={classes.vicoTypeViewTitle}>CREA TU VICO</span>
+          <p className={classes.vicoTypeViewSubtitle}>
+            Escoge el tipo de vivienda que quieres ofrecer.
+          </p>
+          <Grid
+            container
+            spacing={isMediumScreen ? 1 : 10}
+            className={classes.gridContainer}
+          >
+            <Grid item xs={12} md={4}>
+              <VICOHouseTypeItem
+                icon="https://uploads.codesandbox.io/uploads/user/129a52fa-24c5-45b6-8b1e-048cf0197deb/eDLd-shared.png"
+                title="Vivienda Compartida"
+                subtitle="Una casa o un apartamento con varias habitaciones, las cuales se alquilan individualmente."
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <VICOHouseTypeItem
+                icon="https://uploads.codesandbox.io/uploads/user/129a52fa-24c5-45b6-8b1e-048cf0197deb/v1t5-studio.png"
+                title="Apartaestudio"
+                subtitle="Normalmente un apartamento de un ambiente que combina una cocina, sala y dormitorio."
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <VICOHouseTypeItem
+                icon="https://uploads.codesandbox.io/uploads/user/129a52fa-24c5-45b6-8b1e-048cf0197deb/Nt_y-private.png"
+                title="Apartamento privado"
+                subtitle="Una casa o un apartamento con varias habitaciones. Se alquila completo. "
+              />
+            </Grid>
+          </Grid>
+          <div className={classes.noteWrapper}>
+            <span className={classes.note}>
+              Soy el Administrador de un Coliving space comercial
+              {isMobileScreen && (
+                <ArrowForwardIcon className={classes.mobileRightArrow} />
+              )}
+            </span>
+            {!isMobileScreen && <ArrowForwardIcon />}
+          </div>
+        </div>
+        {/* <VICOButton
+          component={RouterLink}
+          to="/create/name"
+          variant="contained"
+          color="primary"
+          text="Continuar"
+          style={{ width: buttonWidth, marginTop: isMediumScreen ? 20 : 40 }}
+        /> */}
+      </div>
+    </div>
   );
 };
 
-export default VicoTypeView;
+export default vicoTypeView;
