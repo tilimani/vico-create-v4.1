c3442ddf80983835fc8cce964d6a72e461fd221f
oumaymaazouz <58980203+oumaymaazouz@users.noreply.github.com>
2020-09-01 22:56:25 +0300

    updates

--- src/components/pages/Create/CreateDashboard/Rules/index.js
+++ src/components/pages/Create/CreateDashboard/Rules/index.js
@@ -1,33 +1,80 @@
 import React from "react";
-import { Button, Grid } from "@material-ui/core";
+import { Button, Grid, Drawer, makeStyles } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
 
+const drawerWidth = 750;
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    marginTop: 20,
+    borderRadius: "12px 0 0 12px"
+  },
+  drawerContent: {
+    width: drawerWidth,
+    flexShrink: 0
+  }
+}));
 const Rules = () => {
+  const classes = useStyles();
+
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
-    <Grid container style={{ margin: "4rem", backgroundColor: "#dadada" }}>
-      <Grid item xs={8}>
-        <div className=".my-first-step">
-          <p>Rules</p>
-          <Button
-            component={RouterLink}
-            to="/create/dashboard/1"
-            variant="contained"
-            color="secondary"
-          >
-            Close Rules
-          </Button>
-          <Button
-            component={RouterLink}
-            to="/create/dashboard/1/services"
-            variant="contained"
-            color="secondary"
-          >
-            Continue
-          </Button>
-        </div>
-      </Grid>
-    </Grid>
+    <Drawer
+      anchor="right"
+      open={true}
+      className={classes.drawer}
+      onClose={() => handleDrawerClose()}
+    >
+      <div className={classes.drawerContent}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </p>
+      </div>
+    </Drawer>
   );
 };
 
 export default Rules;
+
+// <Grid container style={{ margin: "4rem", backgroundColor: "#dadada" }}>
+//   <Grid item xs={8}>
+//     <div className=".my-first-step">
+//       <p>Rules</p>
+//       <Button
+//         component={RouterLink}
+//         to="/create/dashboard/1"
+//         variant="contained"
+//         color="secondary"
+//       >
+//         Close Rules
+//       </Button>
+//       <Button
+//         component={RouterLink}
+//         to="/create/dashboard/1/services"
+//         variant="contained"
+//         color="secondary"
+//       >
+//         Continue
+//       </Button>
+//     </div>
+//   </Grid>
+// </Grid>
--- src/components/pages/Create/CreateDashboard/index.js
+++ src/components/pages/Create/CreateDashboard/index.js
@@ -1,75 +1,99 @@
-import React, { useContext, useState } from "react";
-import {
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions
-} from "@material-ui/core";
+import React, { useState, useContext } from "react";
 import { Link as RouterLink, Route, withRouter } from "react-router-dom";
-import Rules from "./Rules";
-import Services from "./Services";
-import CommonAreas from "./CommonAreas";
-import RoomEdit from "./RoomEdit";
-import { CreateContext } from "../../../../common/context";
+import { makeStyles, Grid, Dialog, DialogContent } from "@material-ui/core";
 import Joyride from "react-joyride";
-import Modal from "@material-ui/core/Modal";
-import VICOButton from "../../../atoms/VICOButton";
-const joyrideSettings = {
-  continuous: true,
-  locale: { next: "Continue", last: "Continue" },
-  disableOverlayClose: true,
-  spotlightClicks: true,
-  styles: {
-    buttonClose: {
-      display: "none"
-    },
-    buttonBack: {
-      display: "none"
-    }
-  }
-};
 
-const TutorialButton = ({ variant, color, onClick, children }) => {
-  return (
-    <Button variant={variant} color={color} onClick={onClick}>
-      {children}
-    </Button>
-  );
-};
+import LeftMenu from "./LeftMenu";
+import Cover from "./Cover";
+import TutorialStep from "./TutorialStep";
+import rules from "../../../../assets/rules.png";
+import socialZones from "../../../../assets/socialZones.png";
+import interests from "../../../../assets/interests.png";
+import description from "../../../../assets/description.png";
+
+import VICOButton from "../../../atoms/VICOButton";
+import { CreateContext } from "../../../../common/context";
+
+import Rules from "./Rules";
+
+const useStyles = makeStyles((theme) => ({
+  leftMenu: { position: "relative", height: "100%" },
+  dashboardContent: {
+    backgroundColor: "#ffffff",
+    position: "relative",
+    height: "100%"
+  },
+  dashboardWrapper: {
+    height: "86vh"
+  },
+  tutorialSteps: {
+    padding: "100px 200px"
+  },
+  tutorialStepGridItem: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  /** dialog */
+  dialogInnerContent: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 362
+  },
+  dialogImg: {
+    width: 100,
+    marginTop: 20
+  },
+  dialogTitle: {
+    fontSize: 20,
+    color: theme.palette.secondary.main,
+    fontWeight: "bold",
+    marginTop: 20,
+    width: "60%",
+    textAlign: "center"
+  },
+  dialogDescription: {
+    fontSize: 16,
+    color: theme.palette.secondary.main,
+    marginTop: 20,
+    width: "80%",
+    textAlign: "center"
+  },
+
+  rulesButton: {
+    cursor: "pointer"
+  }
+}));
 
 const CreateDashboard = (props) => {
+  const classes = useStyles();
+
+  /** Context */
   const { house, createStep, changeState } = useContext(CreateContext);
-  const hasRooms = house.type !== "studio" ? true : false;
-  const [openDatepicker, setOpenDatepicker] = useState(false);
-  const [openScheduled, setOpenScheduled] = useState(false);
+  /** Dialog: VICO is created successfully */
+  const [
+    creationSuccessDialogIsOpen,
+    setCreationSuccessDialogIsOpen
+  ] = useState(true);
+
   const [tutorialOne, setTutorialOne] = useState({});
-  const [tutorialTwo, setTutorialTwo] = useState({});
-  const [tutorialThree, setTutorialThree] = useState({});
-  const [tutorialFour, setTutorialFour] = useState({});
 
-  const nextStep = () => {
-    if (hasRooms) {
-      changeState("createStep", 4);
-    } else {
-      changeState("createStep", 6);
-      setOpenDatepicker(true);
-      // props.history.push(`/create/dashboard/${house.id}`);
+  const joyrideSettings = {
+    continuous: true,
+    locale: { next: "Continue", last: "Continue" },
+    disableOverlayClose: true,
+    spotlightClicks: true,
+    styles: {
+      buttonClose: {
+        display: "none"
+      },
+      buttonBack: {
+        display: "none"
+      }
     }
-  };
-
-  const handleDatepickerClose = () => {
-    setOpenDatepicker(false);
-  };
-
-  const handleScheduledClose = () => {
-    setOpenScheduled(false);
-  };
-
-  const handleScheduleVisit = () => {
-    setOpenDatepicker(false);
-    setOpenScheduled(true);
   };
 
   const [tutorialSteps] = useState({
@@ -91,185 +115,61 @@ const CreateDashboard = (props) => {
           }
         }
       }
-    ],
-    two: [
-      {
-        target: "#rules_button",
-        content: "Gracias por crear tus condiciones!",
-        disableBeacon: true,
-        placement: "bottom"
-      },
-      {
-        target: "#common_areas_button",
-        content: "Zonas sociales",
-        disableBeacon: true,
-        placement: "bottom",
-        styles: {
-          buttonNext: {
-            display: "none"
-          }
-        }
-      },
-      {
-        target: "#common_areas_gallery",
-        content: "Fotos de las Zonas sociales",
-        disableBeacon: true,
-        placement: "left"
-      },
-      {
-        target: "#common_areas_info",
-        content: "Informacion sobre las Zonas sociales",
-        disableBeacon: true,
-        placement: "top",
-        styles: {
-          buttonNext: {
-            display: "none"
-          }
-        }
-      }
-    ],
-    three: [
-      {
-        target: "#common_areas_button",
-        content: (
-          <>
-            Perfecto las zonas sociales fueron actualizadas
-            <VICOButton
-              variant="contained"
-              color="primary"
-              onClick={nextStep}
-              text="Continuar"
-            />
-          </>
-        ),
-        disableBeacon: true,
-        placement: "bottom",
-        styles: {
-          buttonNext: {
-            display: "none"
-          }
-        }
-      }
-    ],
-    four: [
-      {
-        target: "#room_button",
-        content: "Edita la habitacion 1",
-        placement: "bottom",
-        disableBeacon: true,
-        styles: {
-          buttonNext: {
-            display: "none"
-          }
-        }
-      }
     ]
   });
 
   return (
-    <div id="dashboard">
-      Create Dashboard
-      <Button
-        component={RouterLink}
-        to="/create/dashboard/1/rules"
-        variant="contained"
-        color="secondary"
-        id="rules_button"
-        onClick={() => {
-          tutorialOne.close();
-        }}
-      >
-        Rules
-      </Button>
-      <Button
-        component={RouterLink}
-        to="/create/dashboard/1/commonareas"
-        variant="contained"
-        color="secondary"
-        id="common_areas_button"
-        onClick={() => {
-          setTimeout(() => {
-            tutorialTwo.next();
-          }, 300);
-        }}
-      >
-        Common areas
-      </Button>
-      {/* Map for each room */}
-      {hasRooms && (
-        <Button
-          component={RouterLink}
-          to="/create/dashboard/1/room/1"
-          variant="contained"
-          color="secondary"
-          id="room_button"
-          onClick={() => {
-            tutorialFour.close();
-          }}
-        >
-          Room 1
-        </Button>
-      )}
-      <Route path="/create/dashboard/:houseId/rules">
-        <Rules />
-      </Route>
-      <Route path="/create/dashboard/:houseId/services" component={Services} />
-      <Route path="/create/dashboard/:houseId/commonareas">
-        <CommonAreas tutorial={tutorialTwo} />
-      </Route>
-      <Route
-        path="/create/dashboard/:houseId/room/:roomId"
-        component={RoomEdit}
-      />
-      {/* Datepicker Dialog */}
-      <Dialog
-        maxWidth="sm"
-        open={openDatepicker}
-        onClose={handleDatepickerClose}
-        aria-labelledby="max-width-dialog-title"
-      >
-        <DialogTitle id="max-width-dialog-title">
-          Fecha de verificacion
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            En que fecha podrias verificar tu VICO con nuestro equipo?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <VICOButton
-            onClick={handleScheduleVisit}
-            variant="contained"
-            color="primary"
-            text="Agendar mi cita"
-          />
-          <Button onClick={handleDatepickerClose} color="primary">
-            Omitir
-          </Button>
-        </DialogActions>
-      </Dialog>
-      {/* Visit Scheduled Dialog */}
-      <Dialog
-        maxWidth="sm"
-        open={openScheduled}
-        onClose={handleScheduledClose}
-        aria-labelledby="max-width-dialog-title"
-      >
-        <DialogTitle id="max-width-dialog-title"></DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Agendaste la cita de verificacion!
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <VICOButton
-            variant="contained"
-            onClick={handleScheduledClose}
-            color="primary"
-            text="Terminar"
-          />
-        </DialogActions>
-      </Dialog>
+    <>
+      <Grid container spacing={0} className={classes.dashboardWrapper}>
+        <Grid item xs={3} className={classes.leftMenu}>
+          <LeftMenu />
+        </Grid>
+        <Grid item xs={9} className={classes.dashboardContent}>
+          <Cover vicoType="Apartaestudio" />
+
+          <Grid container spacing={0} className={classes.tutorialSteps}>
+            <Grid item xs={6} sm={3} className={classes.tutorialStepGridItem}>
+              <div
+                id="rules_button"
+                className={classes.rulesButton}
+                onClick={() => {
+                  tutorialOne.close();
+                  props.history.push("/create/dashboard/1/rules");
+                }}
+              >
+                <TutorialStep
+                  done={false}
+                  image={rules}
+                  text="Normas de la VICO"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={6} sm={3} className={classes.tutorialStepGridItem}>
+              <TutorialStep
+                done={false}
+                image={socialZones}
+                text="Zonas sociales"
+              />
+            </Grid>
+            <Grid item xs={6} sm={3} className={classes.tutorialStepGridItem}>
+              <TutorialStep
+                done={false}
+                image={interests}
+                text="Puntos de interés"
+              />
+            </Grid>
+            <Grid item xs={6} sm={3} className={classes.tutorialStepGridItem}>
+              <TutorialStep
+                done={false}
+                image={description}
+                text="Descripción de tu VICO"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/** Joyrides */}
       <Joyride
         key={"first-tutorial"}
         steps={tutorialSteps.one}
@@ -279,34 +179,46 @@ const CreateDashboard = (props) => {
         }}
         {...joyrideSettings}
       />
-      <Joyride
-        key={"second-tutorial"}
-        steps={tutorialSteps.two}
-        run={createStep === 2}
-        getHelpers={(helpers) => {
-          setTutorialTwo(helpers);
-        }}
-        {...joyrideSettings}
-      />
-      <Joyride
-        key={"third-tutorial"}
-        steps={tutorialSteps.three}
-        run={createStep === 3}
-        getHelpers={(helpers) => {
-          setTutorialThree(helpers);
-        }}
-        {...joyrideSettings}
-      />
-      <Joyride
-        key={"four-tutorial"}
-        steps={tutorialSteps.four}
-        run={createStep === 4}
-        getHelpers={(helpers) => {
-          setTutorialFour(helpers);
-        }}
-        {...joyrideSettings}
-      />
-    </div>
+
+      {/** Routes */}
+      {/** Rules */}
+      <Route path="/create/dashboard/:houseId/rules">
+        <Rules />
+      </Route>
+      {/** Modal displayed once this dashboard is opened showing that the vico is successfully */}
+      {/* <Dialog
+        open={creationSuccessDialogIsOpen}
+        onClose={() => setCreationSuccessDialogIsOpen(false)}
+      >
+        <DialogContent>
+          <div className={classes.dialogInnerContent}>
+            <img
+              src="https://uploads.codesandbox.io/uploads/user/129a52fa-24c5-45b6-8b1e-048cf0197deb/PUeT-creationSuccess.png"
+              alt=""
+              className={classes.dialogImg}
+            />
+            <span className={classes.dialogTitle}>
+              ¡Felicitaciones! Tu VICO ha sido creada.
+            </span>
+            <p className={classes.dialogDescription}>
+              Queremos que tu VICO se vea de la mejor manera posible. A
+              continuación, vamos a guiarte para que puedas personalizarla.
+            </p>
+            <VICOButton
+              onClick={() => setCreationSuccessDialogIsOpen(false)}
+              variant="contained"
+              color="primary"
+              text="Continuar"
+              style={{
+                width: 267,
+                marginTop: 20,
+                marginBottom: 40
+              }}
+            />
+          </div>
+        </DialogContent>
+      </Dialog> */}
+    </>
   );
 };
 
