8b132f7f841dbe451e38ec0a335cf0d8e445b58a
oumaymaazouz <58980203+oumaymaazouz@users.noreply.github.com>
2020-09-06 13:41:35 +0300

    Fix gallery behaviour

--- src/components/atoms/VICODropZone/index.js
+++ src/components/atoms/VICODropZone/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core";
-
-import Dropzone from "react-dropzone";
+import IconButton from "@material-ui/core/IconButton";
+import CancelIcon from "@material-ui/icons/Cancel";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 
 import upload from "../../../assets/upload.png";
 import VICOButton from "../VICOButton";
@@ -29,33 +30,198 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 16,
     color: theme.palette.secondary.main
   },
-  input: {
+  imagesPreview: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  imageWrapper: {
+    width: 87,
+    height: 80,
+    borderRadius: 6,
+    marginRight: 30,
+    marginTop: 30,
+    position: "relative"
+  },
+  closeIcon: {
+    color: "#C4C4C4",
+    height: 26,
+    width: 26,
+    backgroundColor: "white",
+    borderRadius: "50%"
+  },
+  closeBtn: {
+    position: "absolute",
+    right: -22,
+    top: -22,
+    backgroundColor: "transparent"
+  },
+  image: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    borderRadius: 6
+  },
+  fileInput: {
     display: "none"
+  },
+  addMorePhotosArea: {
+    width: 87,
+    height: 80,
+    marginRight: 30,
+    marginTop: 30,
+    cursor: "pointer",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  addMorePhotosText: {
+    fontSize: 16,
+    color: theme.palette.secondary.main,
+    letterSpacing: "0.02em",
+    textAlign: "center"
+  },
+  addMorePhotosIcon: {
+    color: "#C4C4C4",
+    height: 26,
+    width: 26
   }
 }));
 
-const VICODropZone = () => {
+const VICODropZone = (props) => {
   const classes = useStyles();
 
+  const { images, setImages } = props;
+
+  const dragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const dragEnter = (e) => {
+    e.preventDefault();
+  };
+
+  const dragLeave = (e) => {
+    e.preventDefault();
+  };
+
+  const validateFile = (file) => {
+    const validTypes = ["image/jpeg", "image/jpg", "image/png"];
+    if (validTypes.indexOf(file.type) === -1) {
+      return false;
+    }
+    return true;
+  };
+
+  function readFile(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (res) => {
+        resolve(res.target.result);
+        return reader.result;
+      };
+      reader.onerror = (err) => reject(err);
+    });
+  }
+
+  const handleFiles = async (files) => {
+    const array = [];
+    for (let i = 0; i < files.length; i++) {
+      if (validateFile(files[i])) {
+        const img = await readFile(files[i]);
+        array.push(img);
+      } else {
+        //Display error
+      }
+    }
+    await setImages([...images, ...array]);
+  };
+
+  const fileDrop = (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    if (files) {
+      handleFiles(files);
+    }
+  };
+
   return (
-    <div>
-      <input
-        accept="image/*"
-        className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      <div className={`${classes.dropZone} dropzone`}>
-        <div className={classes.dropZoneIcon}>
-          <img src={upload} alt="upload-icon" />
+    <div className={classes.dropZoneWrapper}>
+      {images.length !== 0 ? (
+        <div className={classes.imagesPreview}>
+          {images.map((img) => (
+            <div className={classes.imageWrapper}>
+              <img src={img} alt="" className={classes.image} />
+              <IconButton
+                className={classes.closeBtn}
+                onClick={() => {
+                  setImages(images.filter((i) => i !== img));
+                }}
+              >
+                <CancelIcon className={classes.closeIcon} />
+              </IconButton>
+            </div>
+          ))}
+          <input
+            accept="image/*"
+            className={classes.fileInput}
+            id="add-more-button-file"
+            multiple
+            type="file"
+            onChange={(e) => {
+              const files = e.target.files;
+              if (files) {
+                handleFiles(files);
+              }
+            }}
+          />
+          <label htmlFor="add-more-button-file">
+            <div className={classes.addMorePhotosArea}>
+              <AddCircleOutlineIcon className={classes.addMorePhotosIcon} />
+              <span className={classes.addMorePhotosText}>Agregar más</span>
+            </div>
+          </label>
         </div>
-        <span className={classes.dropZoneText}>Arrastra las fotos acá</span>
-        <span className={classes.dropZoneText}>o</span>
-        <label className={classes.dropZoneBtn} htmlFor="contained-button-file">
-          <VICOButton text="Subir fotos" variant="outlined" color="primary" />
-        </label>
-      </div>
+      ) : (
+        <div
+          className={`${classes.dropZone} dropzone`}
+          onDragOver={dragOver}
+          onDragEnter={dragEnter}
+          onDragLeave={dragLeave}
+          onDrop={fileDrop}
+        >
+          <div className={classes.dropZoneIcon}>
+            <img src={upload} alt="upload-icon" />
+          </div>
+          <span className={classes.dropZoneText}>Arrastra las fotos acá</span>
+          <span className={classes.dropZoneText}>o</span>
+          <div className={classes.dropZoneBtn}>
+            <input
+              accept="image/*"
+              className={classes.fileInput}
+              id="contained-button-file"
+              multiple
+              type="file"
+              onChange={(e) => {
+                const files = e.target.files;
+                if (files) {
+                  handleFiles(files);
+                }
+              }}
+            />
+            <label htmlFor="contained-button-file">
+              <VICOButton
+                text="Subir fotos"
+                variant="outlined"
+                color="primary"
+                component="span"
+              />
+            </label>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/pages/Create/CreateDashboard/CommonAreas/index.js
+++ src/components/pages/Create/CreateDashboard/CommonAreas/index.js
@@ -1,20 +1,13 @@
-import React, { useContext, useEffect } from "react";
-import {
-  Button,
-  Grid,
-  makeStyles,
-  TextField,
-  withStyles
-} from "@material-ui/core";
+import React, { useContext, useState } from "react";
+import { makeStyles } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
 import { CreateContext } from "../../../../../common/context";
-import CloseIcon from "@material-ui/icons/Close";
-import CheckIcon from "@material-ui/icons/Check";
+
 import RightDrawerScaffold from "../RightDrawerScaffold";
 import VICOButton from "../../../../atoms/VICOButton";
 
 import "./index.css";
-import VICODropZone from "../../../../atoms/VICODropZone";
+
 import VICOTag from "../../../../atoms/VICOTag";
 import VICOSquareBtn from "../../../../atoms/VICOSquareBtn";
 
@@ -48,6 +41,7 @@ import garaje from "../../../../../assets/garaje.png";
 import escritorio from "../../../../../assets/escritorio.png";
 import nevara from "../../../../../assets/nevara.png";
 import espaciodetrabajo from "../../../../../assets/espaciodetrabajo.png";
+import Gallery from "./Gallery";
 
 const useStyles = makeStyles((theme) => ({
   drawerContent: {
@@ -59,43 +53,6 @@ const useStyles = makeStyles((theme) => ({
   },
   additionalContent: {
     marginTop: 40
-  },
-  title: {
-    fontSize: 20,
-    color: theme.palette.secondary.main,
-    fontWeight: "bold"
-  },
-  subtitle: {
-    fontSize: 16,
-    color: theme.palette.secondary.main
-  },
-  tips: {
-    color: theme.palette.secondary.main,
-    marginTop: 46
-  },
-  tipsTitle: {
-    fontSize: 16,
-    color: theme.palette.secondary.main,
-    marginTop: 30,
-    fontWeight: "bold"
-  },
-  tipItem: {
-    display: "flex",
-    marginTop: 10
-  },
-  tipItemCheckIcon: {
-    color: "#07A529",
-    width: 16,
-    marginRight: 10
-  },
-  tipItemCloseIcon: {
-    color: "#D64040",
-    width: 16,
-    marginRight: 10
-  },
-  tipItemText: {
-    fontSize: 16,
-    color: theme.palette.secondary.main
   },
   question: {
     marginTop: 40
@@ -149,17 +106,11 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const CustomTextField = withStyles((theme) => ({
-  root: {
-    height: 81,
-    width: 406,
-    borderRadius: 6,
-    boxShadow: "0px 2px 4px rgba(239, 142, 5, 0.53)"
-  }
-}))(TextField);
-
 const CommonAreas = ({ tutorial, history }) => {
   const classes = useStyles();
+
+  const [images, setImages] = useState([]);
+
   const { changeState } = useContext(CreateContext);
   const handleClick = () => {
     changeState("createStep", 3);
@@ -177,66 +128,9 @@ const CommonAreas = ({ tutorial, history }) => {
     >
       <div>
         <div className={classes.drawerContent}>
-          <div id="common_areas_gallery">
-            <span className={classes.title}>Galeria zonas sociales</span>
-            <p className={classes.subtitle}>
-              Sube mínimo 5 fotos de las zonas sociales y áreas compartidas de
-              tu VICO.
-            </p>
+          <Gallery images={images} setImages={setImages} tutorial={tutorial} />
 
-            <VICODropZone />
-
-            <div className={classes.tips}>
-              <span className={classes.tipsTitle}>Tips rápidos:</span>
-              <div>
-                <div className={classes.tipItem}>
-                  <CheckIcon className={classes.tipItemCheckIcon} />
-                  <span className={classes.tipItemText}>
-                    Estas fotos son para mostrar la habitación#1
-                  </span>
-                </div>
-
-                <div className={classes.tipItem}>
-                  <CheckIcon className={classes.tipItemCheckIcon} />
-                  <span className={classes.tipItemText}>
-                    Ordena los espacios y recoge los objetos del piso y la
-                    cocina.
-                  </span>
-                </div>
-
-                <div className={classes.tipItem}>
-                  <CheckIcon className={classes.tipItemCheckIcon} />
-                  <span className={classes.tipItemText}>
-                    Las fotos horizontales funcionan mejor..
-                  </span>
-                </div>
-
-                <div className={classes.tipItem}>
-                  <CloseIcon className={classes.tipItemCloseIcon} />
-                  <span className={classes.tipItemText}>
-                    Evita fotos oscuras y borrosas.
-                  </span>
-                </div>
-              </div>
-            </div>
-
-            <span className={classes.title}>Adicional</span>
-            <div className={classes.question}>
-              <p className={classes.questionDescription}>
-                ¿Tienes un video de tu VICO?
-              </p>
-              <div className={classes.responseWrapper}>
-                <CustomTextField
-                  placeholder="Ingresa el link"
-                  label="Video"
-                  InputLabelProps={{
-                    shrink: true
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-          <div className={classes.additionalContent}>
+          <div id="common_areas_info" className={classes.additionalContent}>
             <div className={classes.question} style={{ paddingTop: 20 }}>
               <span className={classes.questionTitle}>
                 ¿Cómo definirías tu VICO?
@@ -446,7 +340,7 @@ const CommonAreas = ({ tutorial, history }) => {
               </div>
             </div>
 
-            <div id="common_areas_info" className={classes.continueBtnWrapper}>
+            <div className={classes.continueBtnWrapper}>
               <VICOButton
                 component={RouterLink}
                 to="/create/dashboard/1"
